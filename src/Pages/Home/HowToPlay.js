642e47f2823b293781271f0440ef608d8edcf8a4
Isabelle Travasso <isa.frederico@gmail.com>
2024-04-30 09:28:26 +0200

    How To Play fixed

--- src/Pages/Home/HowToPlay.js
+++ src/Pages/Home/HowToPlay.js
@@ -16,18 +16,68 @@ function HowToPlay() {
         // Backgroung End
 
         // Text Box
-        const box = {
+        let cursors = ['default', 'pointer'];
+
+        const box = [{
+            name: 'box',
             x: width - 530,
             y: height - 350,
             w: width - 130,
-            h: height - 100
+            h: height - 100,
+            cursor: 0
+        },
+        {
+            name: 'button',
+            x: width - 380,
+            y: height - 80,
+            w: width - 430,
+            h: height - 348,
+            cursor: 1
+        }]
+
+        let newCursor;
+
+        function defineBack(b) {
+            if (b.name === 'box') {
+                context.fillStyle = 'rgba(156, 152, 152, 0.8)';
+                context.beginPath();
+                context.roundRect(b.x, b.y, b.w, b.h, 20)
+                context.fill();
+                context.closePath();
+
+                //Text
+                context.font = '17pt Kremlin Pro Web';
+                context.fillStyle = '#000000';
+                var txt = "Your goal is to jump rope, in each stage a charac-\n\nter will be added who will hold the commands res-\n\npectively: z, x, c, v. \n\n\nCoins indicate your number of attempts, which in-\n\ncreases as the stage progresses.\n\n\nThe scoreboard shows you how many jumps are\n\nstill needed to finish the stage.";
+                var x = b.x + 10;
+                var y = b.y + 50;
+                var lineheight = 15;
+                var lines = txt.split('\n');
+
+                for (var i = 0; i < lines.length; i++) {
+                    context.fillText(lines[i], x, y + (i * lineheight))
+                }
+                //Text End
+
+            } else if (b.name === 'button') {
+                context.beginPath();
+                context.rect(b.x, b.y, b.w, b.h)
+                // context.stroke()
+                context.closePath();
+            }
         }
-        context.fillStyle = 'rgba(156, 152, 152, 0.8)';
-        context.beginPath();
-        context.roundRect(box.x, box.y, box.w, box.h, 20)
-        context.fill();
-        context.closePath();
-        // Text Box End
+
+        for (var i = 0; i < box.length; i++) {
+            var b = box[i];
+            defineBack(b);
+
+            if (context.isPointInPath(mouseX, mouseY)) {
+
+                newCursor = box[i].cursor;
+            }
+        }
+
+        //Text Box End
 
         // Title
         const title = {
@@ -39,24 +89,8 @@ function HowToPlay() {
         context.drawImage(U.howToPlayButtonRef, title.x, title.y, title.w, title.h)
         // Title End
 
-        //Text
-        context.font = '17pt Kremlin Pro Web';
-        context.fillStyle = '#000000';
-        var txt = "Your goal is to jump rope, in each stage a charac-\n\nter will be added who will hold the commands res-\n\npectively: z, x, c, v. \n\n\nCoins indicate your number of attempts, which in-\n\ncreases as the stage progresses.\n\n\nThe scoreboard shows you how many jumps are\n\nstill needed to finish the stage.";
-        var x = box.x + 10;
-        var y = box.y + 50;
-        var lineheight = 15;
-        var lines = txt.split('\n');
-
-        for (var i = 0; i < lines.length; i++) {
-            context.fillText(lines[i], x, y + (i * lineheight))
-        }
-        //Text End
 
         //Button
-        let cursors = ['default', 'pointer'];
-        let newCursor;
-
 
         const button = {
             x: width - 400,
@@ -65,14 +99,13 @@ function HowToPlay() {
             h: height - 70,
             cursor: 1
         }
-        if (mouseY > button.x +400 && mouseY < button.x + button.h
-            && mouseX > button.y && mouseX < button.y + button.w) {
-            newCursor = button.cursor;
-        }
-        if (newCursor === undefined) {
+
+        if (newCursor === undefined || newCursor === 0) {
             context.canvas.style.cursor = cursors[0];
         } else if (newCursor === 1) {
             context.canvas.style.cursor = cursors[1];
+            context.drawImage(U.playButtonRef, button.x, button.y, button.w + 5, button.h + 5)
+
         }
 
         context.drawImage(U.playButtonRef, button.x, button.y, button.w, button.h)
@@ -82,7 +115,7 @@ function HowToPlay() {
             let y = e.pageY - offsetY
             if (y > button.x && y < button.x + button.h
                 && x > button.y && x < button.y + button.w) {
-                // requests.renderScreen("Play")
+                requests.renderScreen("Play")
             }
 
         }, false);
